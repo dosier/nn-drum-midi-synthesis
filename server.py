77176226afe8af6cd143c5d59c46669b98d1e952
Stan van der Bend <stanvdbend@gmail.com>
2021-06-22 21:20:42 +0200

    Added support for model loading and making predictions

--- server.py
+++ server.py
@@ -1,5 +1,10 @@
 import socket
 
+from keras.models import load_model
+
+from constants import SAVE_PATH
+from data_load import read_file
+
 HOST = "localhost"
 PORT = 9999
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,5 +21,8 @@ while True:
     conn.send(bytes("Message" + "\r\n", 'UTF-8'))
     print("Message sent")
     data = conn.recv(1024)
-    print(data.decode(encoding='UTF-8'))
+    file_path = data.decode(encoding='UTF-8')
+    x, _ = read_file(file_path)
+    model = load_model(SAVE_PATH)
+    y = model.predict(x)
     conn.send(bytes("BOO" + "\r\n", 'UTF-8'))
