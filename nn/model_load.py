a369e8d30934e0f874103bca125a22c95e9e6c66
Stan van der bend <stanvdbend@gmail.com>
2021-06-30 15:58:09 +0200

    Moved model loading code to model_load.py

---
+++ nn/model_load.py
@@ -0,0 +1,10 @@
+from tensorflow.python.keras.models import model_from_json
+
+
+def load_model_and_weights():
+    json_file = open('model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+    model = model_from_json(loaded_model_json)
+    model.load_weights("model.h5")
+    return model
--- nn/model_predict.py
+++ nn/model_predict.py
@@ -1,19 +1,14 @@
 import numpy
-from tensorflow.python.keras.models import model_from_json
 
+from nn.model_load import load_model_and_weights
 from nn.preprocessing import load_X_Y
-
-json_file = open('model.json', 'r')
-loaded_model_json = json_file.read()
-json_file.close()
-model = model_from_json(loaded_model_json)
-model.load_weights("model.h5")
 
 X, Y = load_X_Y(True, 16, 16)
 
 print("Size of X {}".format(X.shape))
 print("Size of Y {}".format(Y.shape))
 
+model = load_model_and_weights()
 print(numpy.around(model.predict(numpy.array([X[0]])), 3)[0])
 print(numpy.around(model.predict(numpy.array([X[0]])), 0)[0])
 print(Y[0])
